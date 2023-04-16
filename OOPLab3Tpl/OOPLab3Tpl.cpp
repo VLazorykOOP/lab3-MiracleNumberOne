25af7eb2be3e32b67e9a71f144fef148a64a4004
Miracle179 <prsky.ivan@gmail.com>
2023-04-16 21:28:40 +0200

    Клас Date — це клас, який представляє дату з компонентами дня, місяця та року. Він має функції для встановлення та отримання значень дня, місяця та року з перевіркою введення, а також функції для друку дати в різних форматах. Клас також містить конструктори для створення об’єктів Date з різними початковими значеннями, включаючи конструктор за замовчуванням, який встановлює дату 1 січня 2023 року, якщо не надано аргументів.
    Клас Date має такі функції:
    
    setDay(int day): встановлює компонент дня дати з перевіркою введення, щоб переконатися, що значення знаходиться в діапазоні від 1 до 31.
    setMonth(int month): встановлює компонент місяця для дати з перевіркою введення, щоб переконатися, що значення знаходиться в діапазоні від 1 до 12.
    setYear(int year): встановлює компонент року для дати.
    getDay() const: Повертає компонент дня дати.
    getMonth() const: Повертає компонент місяця дати.
    getYear() const: повертає компонент року дати.
    printDate1() const: друкує дату у форматі «день місяць рік», де місяць відображається як відповідна назва місяця (наприклад, «січень»).
    printDate2() const: друкує дату у форматі "день.місяць.рік", з нулями на початку для дня та місяця.
    Клас Date також містить конструктори для створення об’єктів Date з різними початковими значеннями, зокрема:
    
    Конструктор за замовчуванням: встановлює дату 1 січня 2023 року.
    Параметризований конструктор: приймає день, місяць і рік як аргументи та встановлює відповідну дату.

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -1,24 +1,85 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №3. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+class Date {
+private:
+    int day;
+    int month;
+    int year;
 
-// Ваші файли загловки 
-//
-#include "Lab3Example.h"
-int main()
-{
-    std::cout << " Lab #3  !\n";
-//  Код виконання завдань
-//  Головне меню завдань
-//  Функції та класи можуть знаходитись в інших файлах проекту
+public:
+    // Constructors
+    Date() : day(1), month(1), year(2023) {}
+    Date(int day, int month, int year) : day(day), month(month), year(year) {}
 
-    int conv = 1;
-    if (conv == 1) conv = mainExample1();
-    if (conv == 1) conv = mainExample3(); 
-    if (conv == 1) conv = mainExample4();
+    // Member functions for setting day, month, and year with input validation
+    void setDay(int day) {
+        if (day < 1 || day > 31) {
+            std::cout << "Error! Invalid day value." << std::endl;
+            return;
+        }
+        this->day = day;
+    }
 
+    void setMonth(int month) {
+        if (month < 1 || month > 12) {
+            std::cout << "Error! Invalid month value." << std::endl;
+            return;
+        }
+        this->month = month;
+    }
+
+    void setYear(int year) {
+        this->year = year;
+    }
+
+    // Member functions for getting day, month, and year
+    int getDay() const {
+        return day;
+    }
+
+    int getMonth() const {
+        return month;
+    }
+
+    int getYear() const {
+        return year;
+    }
+
+    // Member functions for printing the date in "day month year" and "day.month.year" formats
+    void printDate1() const {
+        std::string monthNames[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        std::cout << day << " " << monthNames[month - 1] << " " << year << " year" << std::endl;
+    }
+
+    void printDate2() const {
+        std::cout << (day < 10 ? "0" : "") << day << "." << (month < 10 ? "0" : "") << month << "." << year << std::endl;
+    }
+};
+
+int main() {
+    // Testing the Date class
+    Date d1; // Using the default constructor, date will be January 1, 2023
+    std::cout << "Date 1: ";
+    d1.printDate1();
+    std::cout << "Date 2: ";
+    d1.printDate2();
+
+    Date d2(5, 1, 2019); // Using the parameterized constructor, date will be January 5, 2019
+    std::cout << "Date 3: ";
+    d2.printDate1();
+    std::cout << "Date 4: ";
+    d2.printDate2();
+
+    // Testing setting day, month, and year
+    d1.setDay(15);
+    d1.setMonth(6);
+    d1.setYear(2022);
+    std::cout << "Updated Date 1: ";
+    d1.printDate1();
+    std::cout << "Updated Date 2: ";
+    d1.printDate2();
+
+    return 0;
 }
 
