2580387258d41d0c6762aaf15ef3d32d73c71fa1
Miracle179 <prsky.ivan@gmail.com>
2023-04-17 15:28:12 +0200

    Клас Vector має: покажчик на масив цілих чисел, який містить значення вектора. розмір: ціле число, яке представляє розмір або довжину вектора. стан: ціле число, яке представляє стан або стан вектора, яке можна використовувати для обробки помилок.
    Клас Vector має такі функції:
    Конструктори: клас надає кілька конструкторів для створення об’єктів Vector. Конструктори — це спеціальні функції-члени, які викликаються під час створення об’єкта класу.
    Деструктор: клас надає деструктор, який є спеціальною функцією-членом, яка викликається, коли об’єкт класу знищується. Деструктор очищає будь-які ресурси, виділені об'єктом, наприклад пам'ять.
    Оператор присвоєння: клас надає оператор присвоювання, який є спеціальною функцією-членом, яка дозволяє об’єктам класу присвоювати значення з інших об’єктів того самого класу.
    Функції Setter і Getter: клас надає функції setter і getter для встановлення та отримання значень вектора за певними індексами.
    Функція друку: клас забезпечує функцію друку, яка друкує значення вектора.
    Арифметичні операції: клас надає оператори для виконання арифметичних операцій над векторами, таких як додавання, віднімання та скалярне множення.

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -57,6 +57,182 @@ public:
     }
 };
 
+
+class Vector {
+private:
+    int* pointer;
+    int size;
+    int state;
+
+public:
+    static int numObjects;
+
+    // Constructors
+    Vector() {
+        pointer = new int[1];
+        size = 1;
+        pointer[0] = 0;
+        state = 0;
+        numObjects++;
+    }
+
+    Vector(int n) {
+        pointer = new int[n];
+        size = n;
+        for (int i = 0; i < n; i++) {
+            pointer[i] = 0;
+        }
+        state = 0;
+        numObjects++;
+    }
+
+    Vector(int n, int value) {
+        pointer = new int[n];
+        size = n;
+        for (int i = 0; i < n; i++) {
+            pointer[i] = value;
+        }
+        state = 0;
+        numObjects++;
+    }
+
+    Vector(const Vector& other) {
+        size = other.size;
+        pointer = new int[size];
+        for (int i = 0; i < size; i++) {
+            pointer[i] = other.pointer[i];
+        }
+        state = other.state;
+        numObjects++;
+    }
+
+    // Destructor
+    ~Vector() {
+        delete[] pointer;
+        numObjects--;
+    }
+
+    // Assignment operator
+    Vector& operator=(const Vector& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        delete[] pointer;
+        size = other.size;
+        pointer = new int[size];
+        for (int i = 0; i < size; i++) {
+            pointer[i] = other.pointer[i];
+        }
+        state = other.state;
+        return *this;
+    }
+
+    // Setter function
+    void setValue(int index, int value = 0) {
+        if (index < 0 || index >= size) {
+            state = -1;
+            return;
+        }
+        pointer[index] = value;
+    }
+
+    // Getter function
+    int getValue(int index) {
+        if (index < 0 || index >= size) {
+            state = -1;
+            return 0;
+        }
+        return pointer[index];
+    }
+
+    // Print function
+    void print() {
+        for (int i = 0; i < size; i++) {
+            std::cout << pointer[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Arithmetic operations
+    Vector operator+(const Vector& other) {
+        Vector result(size);
+        for (int i = 0; i < size; i++) {
+            result.pointer[i] = pointer[i] + other.pointer[i];
+        }
+        return result;
+    }
+
+    Vector operator-(const Vector& other) {
+        Vector result(size);
+        for (int i = 0; i < size; i++) {
+            result.pointer[i] = pointer[i] - other.pointer[i];
+        }
+        return result;
+    }
+
+    Vector operator*(short value) {
+        Vector result(size);
+        for (int i = 0; i < size; i++) {
+            result.pointer[i] = pointer[i] * value;
+        }
+        return result;
+    }
+// Comparison operations
+    bool operator>(const Vector& other) {
+        for (int i = 0; i < size; i++) {
+            if (pointer[i] <= other.pointer[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator<(const Vector& other) {
+        for (int i = 0; i < size; i++) {
+            if (pointer[i] >= other.pointer[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator==(const Vector& other) {
+        for (int i = 0; i < size; i++) {
+            if (pointer[i] != other.pointer[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Vector& other) {
+        return !(*this == other);
+    }
+
+    // Static member variable
+    static int getNumObjects() {
+        return numObjects;
+    }
+
+private:
+    // Helper function to display error messages
+    void displayError() {
+        std::cerr << "Error: ";
+        if (state == -1) {
+            std::cerr << "Index out of range." << std::endl;
+        }
+        else {
+            std::cerr << "Unknown error." << std::endl;
+        }
+    }
+
+};
+
+// Initialize static member variable
+int Vector::numObjects = 0;
+
+
 int main() {
     // Testing the Date class
     Date d1; // Using the default constructor, date will be January 1, 2023
@@ -80,6 +256,61 @@ int main() {
     std::cout << "Updated Date 2: ";
     d1.printDate2();
 
+
+    // Test the Vector class
+    Vector v1; // Default constructor
+    Vector v2(5); // Constructor with size argument
+    Vector v3(3, 2); // Constructor with size and value arguments
+
+    // Assignment operator
+    v1 = v3;
+    v1.print(); // Expected output: 2 2 2
+
+    // Setter and getter functions
+    v2.setValue(2, 10);
+    std::cout << v2.getValue(2) << std::endl; // Expected output: 10
+
+    // Arithmetic operations
+    Vector v4 = v2 + v3;
+    v4.print(); // Expected output: 12 12 12 2 2
+
+    Vector v5 = v4 - v3;
+    v5.print(); // Expected output: 10 10 10 0 0
+
+    Vector v6 = v3 * 3;
+    v6.print(); // Expected output: 6 6 6
+
+    // Comparison operations
+    if (v2 > v3) {
+        std::cout << "v2 is greater than v3" << std::endl;
+    }
+    else {
+        std::cout << "v2 is not greater than v3" << std::endl;
+    }
+
+    if (v2 < v3) {
+        std::cout << "v2 is less than v3" << std::endl;
+    }
+    else {
+        std::cout << "v2 is not less than v3" << std::endl;
+    }
+
+    if (v2 == v3) {
+        std::cout << "v2 is equal to v3" << std::endl;
+    }
+    else {
+        std::cout << "v2 is not equal to v3" << std::endl;
+    }
+
+    if (v2 != v3) {
+        std::cout << "v2 is not equal to v3" << std::endl;
+    }
+    else {
+        std::cout << "v2 is equal to v3" << std::endl;
+    }
+
+    // Static member variable
+    std::cout << "Number of Vector objects created: " << Vector::getNumObjects() << std::endl;
+
     return 0;
 }
-
